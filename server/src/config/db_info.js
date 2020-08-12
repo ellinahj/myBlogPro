d13c8ba489f2a67e775a6eb3ddb65bd463fbc405
Hyeonjeong Park <ellinahj@gmail.com>
2020-08-13 03:31:06 +0900

    re deploy test

---
+++ server/src/config/db_info.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pro: {
+    host: "myblogdb.cbl9z0u2kths.us-east-1.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "rdsdpfflsk1?",
+    database: "mysql"
+  },
+  dev: {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "myadmin!?",
+    database: "mydiary"
+  },
+  test: {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "myadmin!?",
+    database: "mydiary"
+  }
+};
