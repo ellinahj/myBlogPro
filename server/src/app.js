15b1b8949ea881a51620c7c5d8a8322a05b9bed4
Hyeonjeong Park <ellinahj@gmail.com>
2020-08-20 19:06:14 +0900

    Set: cors

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: "http://hyungjung.site",
   credentials: true
 };
 app.use(cors(corsOptions));
