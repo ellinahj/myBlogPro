f40be27a374fa11da12cece5dbca3523cfc6312d
Hyeonjeong Park <ellinahj@gmail.com>
2020-08-23 22:27:50 +0900

    Set: meta og

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -18,6 +18,14 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
           />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://hyunjung.site" />
+          <meta property="og:title" content="MyBlog" />
+          <meta property="og:description" content="나의 스타일을 표현하는 블로그" />
+          <meta
+            property="og:image"
+            content="https://myblogs3.s3.ap-northeast-2.amazonaws.com/images/KakaoTalk_Photo_2020-08-23-22-15-05.jpeg"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;700&family=Nanum+Myeongjo:wght@400;700&display=swap"
             rel="stylesheet"
